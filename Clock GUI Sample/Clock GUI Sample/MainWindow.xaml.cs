61dda56d5e6f0859274fa7294942fce93cd8880b
MazaiCrafty <takatim1574@icloud.com>
2017-11-19 17:45:52 +1030

    Add files via upload

---
+++ Clock GUI Sample/Clock GUI Sample/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Threading;
+using System.Windows.Shapes;
+
+namespace Clock_GUI_Sample
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// 時刻表示タイマー
+        /// </summary>
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = CreateTimer();
+        }
+
+        /// <summary>
+        /// タイマー生成処理
+        /// </summary>
+        /// <returns>生成したタイマー</returns>
+        private DispatcherTimer CreateTimer()
+        {
+            var t = new DispatcherTimer(DispatcherPriority.System.Idle);
+            t.Interval = TimeSpan.FromMilliseconds(300);
+            t.Tick += (sender, e) =>
+            {
+                // 処理
+
+                // 時刻をテキストに設定
+                TextBlock.Text = DateTime.Now.ToString("HH:mm:ss");
+            };
+
+            return t;
+        }
+    }
+}
